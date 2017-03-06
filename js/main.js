f3254173b69de3a0a0c08c65397f778515737559
Kate Zellmer <kezellmer@wisc.edu>
2017-03-06 11:30:32 -0600

    Legend Update
    Added Legend

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
-
 // create the map
 var map = L.map('map').setView([40, -95], 3);
 
     L.tileLayer('https://api.mapbox.com/styles/v1/katezellmer/ciuyj1kpx00hd2js59nk6alff/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoia2F0ZXplbGxtZXIiLCJhIjoiY2l1dDZpMHhkMDBrMTJ0bjBkNThmcDRtcCJ9.a7-sXy-HPat2xCkGnlKmJw', {
-    maxZoom: 15
+    maxZoom: 10
 }).addTo(map);
 
 //call getData function
@@ -43,14 +42,7 @@ function pointToLayer(feature, latlng, attributes) {
       //create circle marker layer
     var layer = L.circleMarker(latlng, geojsonMarkerOptions);
 
-    //build popup content string starting with city...Example 2.1 line 24
-    var popupContent = "<p><b>City:</b> " + feature.properties.City + "</p>";
-
-    //add formatted attribute to popup content string
-    var year = attribute.split("_")[1];
-    popupContent += "<p><b>Population in " + year + ":</b> " + feature.properties[attribute] + " homeless</p>";
-    //bind the popup to the circle marker
-    layer.bindPopup(popupContent);
+    createPopup(feature.properties, attribute, layer, geojsonMarkerOptions.radius);
 
     //return the circle marker to the L.geoJson pointToLayer option
     return layer;
@@ -65,15 +57,36 @@ function createPropSymbols(data, map, attributes){
             return pointToLayer(feature, latlng, attributes);
         } 
     }).addTo(map);
-    implementSearch(data, featuresLayer);
+    implementSearch(data, map, featuresLayer);
 }
 
-//Step 1: Create new sequence controls
 function createSequenceControls(map, attributes){
-    //create range input element (slider)
-    $('#panel').append('<input class="range-slider" type="range">');
-    $('#panel').append('<button class="skip" id="reverse">Reverse</button>');
-    $('#panel').append('<button class="skip" id="forward">Skip</button>');
+        var SequenceControl = L.Control.extend({
+        options: {
+            position: 'bottomleft'
+        },
+
+        onAdd: function (map) {
+            // create the control container div with a particular class name
+            var container = L.DomUtil.create('div', 'sequence-control-container');
+
+            //create range input element (slider)
+            $(container).append('<input class="range-slider" type="range">');
+            //add skip buttons
+            $(container).append('<button class="skip" id="reverse" title="Reverse">Reverse</button>');
+            $(container).append('<button class="skip" id="forward" title="Forward">Skip</button>');
+
+            //kill any mouse event listeners on the map
+            $(container).on('mousedown dblclick', function(e){
+                L.DomEvent.stopPropagation(e);
+            });
+
+            return container;
+        }
+    });
+
+    map.addControl(new SequenceControl());
+
     $('#reverse').html('<img src="img/rewind.png">');
     $('#forward').html('<img src="img/forward.png">');
         //set slider attributes
@@ -118,6 +131,104 @@ function createSequenceControls(map, attributes){
     });
 };
 
+function createLegend(map, attributes) {
+    var LegendControl = L.Control.extend({
+        options: {
+            position: 'bottomright'
+        },
+
+        onAdd: function (map) {
+            // create the control container with a particular class name
+            var container = L.DomUtil.create('div', 'legend-control-container');
+
+            //add temporal legend div to container
+            $(container).append('<div id="temporal-legend">')
+
+            //Step 1: start attribute legend svg string
+            var svg = '<svg id="attribute-legend" width="160px" height="60px">';
+
+            //array of circle names to base loop on
+            var circles = ["max", "mean", "min"];
+
+            //Step 2: loop to add each circle and text to svg string
+            for (var i=0; i<circles.length; i++){
+                //circle string
+                svg += '<circle class="legend-circle" id="' + circles[i] + 
+                '" fill="#F47821" fill-opacity="0.8" stroke="#000000" cx="30"/>';
+            };
+
+            //close svg string
+            svg += "</svg>";
+
+            //add attribute legend svg to container
+            $(container).append(svg);
+
+            return container;
+        }
+    });
+
+    map.addControl(new LegendControl());
+
+    updateLegend(map, attributes[0]);
+}
+
+//Calculate the max, mean, and min values for a given attribute
+function getCircleValues(map, attribute){
+    //start with min at highest possible and max at lowest possible number
+    var min = Infinity,
+        max = -Infinity;
+
+    map.eachLayer(function(layer){
+        //get the attribute value
+        if (layer.feature){
+            var attributeValue = Number(layer.feature.properties[attribute]);
+
+            //test for min
+            if (attributeValue < min){
+                min = attributeValue;
+            };
+
+            //test for max
+            if (attributeValue > max){
+                max = attributeValue;
+            };
+        };
+    });
+
+    //set mean
+    var mean = (max + min) / 2;
+
+    //return values as an object
+    return {
+        max: max,
+        mean: mean,
+        min: min
+    };
+};
+
+//Update the legend with new attribute
+function updateLegend(map, attribute){
+    //create content for legend
+    var year = attribute.split("_")[1];
+    var content = "<b>Number Homeless in " + year;
+
+    //replace legend content
+    $('#temporal-legend').html(content);
+
+    //get the max, mean, and min values as an object
+    var circleValues = getCircleValues(map, attribute);
+
+    for (var key in circleValues){
+        //get the radius
+        var radius = calcPropRadius(circleValues[key]);
+
+        //Step 3: assign the cy and r attributes
+        $('#'+key).attr({
+            cy: 59 - radius,
+            r: radius
+        });
+    }
+};
 //Step 10: Resize proportional symbols according to new attribute values
 function updatePropSymbols(map, attribute){
     map.eachLayer(function(layer){
@@ -128,19 +239,25 @@ function updatePropSymbols(map, attribute){
             //update each feature's radius based on new attribute values
             var radius = calcPropRadius(props[attribute]);
             layer.setRadius(radius);
+            
+            createPopup(props, attribute, layer, radius);
+            updateLegend(map, attribute);
 
-            //add city to popup content string
-            var popupContent = "<p><b>City:</b> " + props.City + "</p>";
-
-            //add formatted attribute to panel content string
-            var year = attribute.split("_")[1];
-            popupContent += "<p><b>Population in " + year + ":</b> " + props[attribute] + " million</p>";
-
-            //replace the layer popup
-            layer.bindPopup(popupContent, {
-                offset: new L.Point(0,-radius)
-            });
         };
+    });
+};
+
+function createPopup(properties, attribute, layer, radius){
+    //add city to popup content string
+    var popupContent = "<p><b>City:</b> " + properties.City + "</p>";
+
+    //add formatted attribute to panel content string
+    var year = attribute.split("_")[1];
+    popupContent += "<p><b>Homeless Population " + year + ":</b> " + properties[attribute] + " homeless</p>";
+
+    //replace the layer popup
+    layer.bindPopup(popupContent, {
+        offset: new L.Point(0,-radius)
     });
 };
 
@@ -165,14 +282,14 @@ function processData(data){
     return attributes;
 };
 
-function implementSearch(data, currLayer) {
+function implementSearch(data, map, currLayer) {
         var searchControl = new L.Control.Search({
                 layer: currLayer,
                 propertyName: 'City',
                 marker: false,
-                moveToLocation: function(latlng, title, map) {
+                moveToLocation: function(latlng, title, mymap) {
                     //map.fitBounds( latlng.layer.getBounds() );
-                    var zoom = map.getBoundsZoom(latlng.layer.getBounds());
+                    var zoom = mymap.getBoundsZoom(latlng.layer.getBounds());
                     map.setView(latlng, zoom); // access the zoom
                 }
         });
@@ -186,7 +303,7 @@ function implementSearch(data, currLayer) {
                         currLayer.resetStyle(layer);
                 });
         });
-                
+             
         map.addControl(searchControl);  //inizialize search control
 }
 
@@ -200,6 +317,8 @@ function getData(map){
             //call function to create proportional symbols
             createPropSymbols(response, map, attributes);
             createSequenceControls(map, attributes);
+            createLegend(map, attributes);
+            
         }
     });
 };
